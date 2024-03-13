f17ea7f98c4a78f5f77a8112417ae099c46b20fd
León Abascal Villa <labasv00@estudiantes.unileon.es>
2024-03-13 17:13:15 +0100

    Nuevo método para probar las inyecciones noSQL

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
-//añadimos mongoose
+
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var sanitize = require('mongo-sanitize');
+
 //añadimos el modelo
 var Device = require('./models/device');
 var app = express();
@@ -29,14 +31,14 @@ app.get('/devices/:type', async (req, res) => {
     try {
         console.log(JSON.stringify(req.params, null, 2));
         // Realiza la búsqueda con un filtro, por ejemplo, {"type": "temperature"}
-    const devices = await Device.find(req.params).exec();
-    res.json(devices);
-} catch (err) {
-    res.status(500).json({
-        msg: 'Error al buscar dispositivos',
-        error: err
-    });
-}
+        const devices = await Device.find(req.params).exec();
+        res.json(devices);
+    } catch (err) {
+        res.status(500).json({
+            msg: 'Error al buscar dispositivos',
+            error: err
+        });
+    }
 });
 
 app.post('/devices', async function (req, res) {
@@ -64,7 +66,16 @@ app.put('/devices/:_id', async (req, res) => {
     try {
         // Busca el dispositivo por type y lo actualiza con los datos del request
         // Una mejor opcion es usar "ID" se usa findByIdAndUpdate,
-        const device = await Device.findByIdAndUpdate(req.params, req.body).exec();
+        let device = await Device.findById(req.params);
+
+        if (device == null) {
+            return res.status(404).json({
+                msg: "No se ha encontrado el dispostivo"
+            })
+        }
+
+        device = await Device.findByIdAndUpdate(req.params, req.body).exec();
+
         res.json({
             msg: 'Dispositivo actualizado correctamente',
             device: device
@@ -80,13 +91,22 @@ app.put('/devices/:_id', async (req, res) => {
 //Peticion DELETE para borrar un dispositivo
 app.delete('/devices/:_id', async (req, res) => {
     try {
-        console.log(JSON.stringify(req.params, null, 2));
+        // console.log(JSON.stringify(req.params, null, 2));
         // Busca el dispositivo por type y lo borra
         const device = await Device.findByIdAndDelete(req.params).exec();
-        res.json({
-            msg: 'Dispositivo borrado correctamente',
-            device: device
-        });
+
+        if (device == null) {
+            return res.status(400).json({
+                msg: "No se ha encontrado el dispositivo"
+            })
+        } else {
+            res.json({
+                msg: 'Dispositivo borrado correctamente',
+                device: device
+            });
+        }
+
+
     } catch (err) {
         res.status(500).json({
             msg: 'Error al borrar el dispositivo',
@@ -94,6 +114,29 @@ app.delete('/devices/:_id', async (req, res) => {
         });
     }
 });
+
+app.post("/devices/find", async (req, res) => {
+    try {
+        console.log(req.body)
+        const clean = sanitize(req.body);
+        console.log(clean)
+        const devices = await Device.find(clean).exec();
+
+        if (devices == null) {
+            return res.status(404).json({
+                msg: "No se han encontrado datos"
+            })
+        } else {
+            res.json(devices);
+        }
+    } catch (err) {
+        console.log("ERROR PROCESANDO LA PETICIÓN")
+        console.log(err)
+        res.status(500).json({
+            msg: "Búsqueda no válida"
+        })
+    }
+})
 
 // app.listen para arrancar el servidor web con express en el puerto 3000
 app.listen(port, function () {
